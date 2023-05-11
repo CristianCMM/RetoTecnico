207ad78a3fb1a5e8c6083a1840e03a968b24d93a
Cristian <c.amiblu@hotmail.com>
2023-05-11 18:10:17 -0500

    Ajustes

--- Reto Tecnico/UtestAutomatizacion/src/main/java/com/UTest/tasks/DevicesTask.java
+++ Reto Tecnico/UtestAutomatizacion/src/main/java/com/UTest/tasks/DevicesTask.java
@@ -4,17 +4,21 @@ import com.UTest.interfazDeUsuario.Complete;
 import com.UTest.interfazDeUsuario.Devices;
 import com.UTest.interfazDeUsuario.Personal;
 import com.UTest.iteracciones.TiempoEspera;
+import net.serenitybdd.core.pages.WebElementState;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.SendKeys;
+import net.serenitybdd.screenplay.matchers.WebElementStateMatchers;
+import net.serenitybdd.screenplay.waits.Wait;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class DevicesTask implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
-           /*     Click.on(Devices.LIST_YOURCOMPUTER),
+                Click.on(Devices.LIST_YOURCOMPUTER),
                 Click.on(Devices.SELEC_YOURCOMPUTER),
                 Click.on(Devices.LIST_VERSION),
                 Click.on(Devices.SELEC_VERSION),
@@ -24,11 +28,12 @@ public class DevicesTask implements Task {
                 Click.on(Devices.SELEC_YOURMOBILE),
                 Click.on(Devices.LIST_MODEL),
                 TiempoEspera.unMomento(2),
+                WaitUntil.the(Devices.SELEC_MODEL, WebElementStateMatchers.isVisible()).forNoMoreThan(10).seconds(),
                 Click.on(Devices.SELEC_MODEL),
-                TiempoEspera.unMomento(2),
                 Click.on(Devices.LIST_OPERTINGSYSTEM),
+                WaitUntil.the(Devices.SELEC_OPERTINGSYSTEM, WebElementStateMatchers.isVisible()).forNoMoreThan(10).seconds(),
+                Click.on(Devices.SELEC_OPERTINGSYSTEM),
                 TiempoEspera.unMomento(2),
-                Click.on(Devices.SELEC_OPERTINGSYSTEM),*/
                 Click.on(Devices.BTN_LASTSTEP)
         );
 
